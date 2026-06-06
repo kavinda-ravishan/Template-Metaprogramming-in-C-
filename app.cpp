ac542a83f2b8fae561ce37a93febcd8fcc5ae8f4
kavinda-ravishan <kvnravishan@outlook.com>
2026-06-06 22:08:26 +0530

    E4

--- app.cpp
+++ app.cpp
@@ -1,72 +1,131 @@
 #include <iostream>
 #include <tuple>
 #include <vector>
+#include <list>
 #include <string>
 
-// void print_func() {}
 
-// template<typename THIS>
-// void print_func(THIS _this) {
-//     std::cout << _this << "\n";
-// }
+template<bool condition, typename THEN, typename ELSE>
+struct if_ {};
 
-// template<typename THIS, typename... REST>
-// void print_func(THIS _this, REST... _rest) {
+template<typename THEN, typename ELSE>
+struct if_<true, THEN, ELSE> {
+    using type = THEN;
+};
 
-//     std::cout << _this << ", ";
-//     print_func(_rest...); // print_func(rest1, rest2, ...)
-// }
+template<typename THEN, typename ELSE>
+struct if_<false, THEN, ELSE> {
+    using type = ELSE;
+};
 
-// template<typename TUPLE, size_t... index_seqence>
-// void print_tuple_impl(TUPLE t, std::index_sequence<index_seqence...>) {
-//     print_func(std::get<index_seqence>(t)...); // print_func(std::get<0>(t), std::get<1>(t), ...)
-// }
+bool contains(const std::string& str, std::list<std::string> list) {
 
-// template<typename TUPLE>
-// void print_tuple(TUPLE t) {
-//     print_tuple_impl(t, std::make_index_sequence<std::tuple_size<TUPLE>::value>{});
-// }
-
-// =========================================
-
-void print_func() {}
-
-template<typename THIS>
-void print_func(THIS&& _this) {
-    std::cout << "Rred -> " << std::forward<THIS>(_this) << "\n";
+    if(list.empty()) return false;
+    else {
+        if(list.front() == str) return true;
+        else {
+            list.pop_front();
+            return contains(str, list);
+        }
+    }
 }
 
-template<typename THIS>
-void print_func(THIS& _this) {
-    std::cout << "Lref -> " << std::forward<THIS>(_this) << "\n";
-}
+template<typename... T>
+struct type_list
+{};
 
-template<typename THIS, typename... REST>
-void print_func(THIS&& _this, REST&&... _rest) {
+template<typename LIST>
+struct empty: std::false_type
+{};
 
-    std::cout << std::forward<THIS>(_this) << ", ";
-    print_func(std::forward<REST>(_rest)...);
-}
+template<>
+struct empty<type_list<>>: std::true_type
+{};
 
-template<typename TUPLE, size_t... index_seqence>
-void print_tuple_impl(TUPLE&& t, std::index_sequence<index_seqence...>) {
-    print_func(std::get<index_seqence>(std::forward<TUPLE>(t))...);
-}
+template<typename LIST>
+struct front
+{};
 
-template<typename TUPLE>
-void print_tuple(TUPLE&& t) {
-    print_tuple_impl(std::forward<TUPLE>(t), std::make_index_sequence<std::tuple_size<std::remove_reference<TUPLE>::type>::value>{});
-}
+template<typename T_THIS, typename... T_REST> // T_REST can be empty
+struct front<type_list<T_THIS, T_REST...>>
+{
+    using type = T_THIS;
+};
 
-// =========================================
+template<typename LIST>
+struct pop_front
+{};
 
+template<typename T_THIS, typename... T_REST>
+struct pop_front<type_list<T_THIS, T_REST...>>
+{
+    using type = type_list<T_REST...>;
+};
+
+template<typename LIST>
+using front_t = typename front<LIST>::type;
+
+template<typename LIST>
+using pop_front_t = typename pop_front<LIST>::type;
+
+template<typename LIST>
+static constexpr bool empty_v = empty<LIST>::value;
+
+// template<typename SEARCH, typename LIST>
+// struct contains_type;
+
+// template<typename SEARCH, typename LIST>
+// struct non_empty_contains_type :
+// if_<
+//     (std::is_same_v<SEARCH, front_t<LIST>>), 
+//     // THEN
+//     std::true_type, 
+//     // ELSE
+//     contains_type<SEARCH, pop_front_t<LIST>>
+// >::type
+// {};
+
+// template<typename SEARCH, typename LIST>
+// struct contains_type :
+// if_<
+//     empty_v<LIST>, 
+//     // THEN
+//     std::false_type, 
+//     // ELSE
+//     non_empty_contains_type<SEARCH, LIST>
+//     >::type
+// {};
+
+template<typename SEARCH, typename LIST>
+struct contains_type :
+if_<
+    (std::is_same_v<SEARCH, front_t<LIST>>), 
+    // THEN
+    std::true_type, 
+    // ELSE
+    contains_type<SEARCH, pop_front_t<LIST>>
+>::type
+{};
+
+template<typename SEARCH>
+struct contains_type<SEARCH, type_list<>> : std::false_type
+{};
+
+template<typename SEARCH, typename LIST>
+static constexpr bool contains_type_v = contains_type<SEARCH, LIST>::value;
 
 int main() {
 
-    print_func("hello", 9, false);
+    const std::list<std::string> list{"int", "bool", "string"};
 
-    const std::tuple t = std::make_tuple(1, 1.23f, "world");
-    print_tuple(std::move(t));
+    std::cout << std::boolalpha << contains("int", list) << "\n";
+    std::cout << std::boolalpha << contains("double", list) << "\n";
+
+    type_list<int, bool, std::string> types;
+    std::cout << std::boolalpha << contains_type_v<int, decltype(types)> << "\n";
+    std::cout << std::boolalpha << contains_type_v<bool, decltype(types)> << "\n";
+    std::cout << std::boolalpha << contains_type_v<double, decltype(types)> << "\n";
+    std::cout << std::boolalpha << contains_type_v<double, type_list<>> << "\n";
 
     return 0;
 }
